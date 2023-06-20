25719f15f2e16c0e6506cfc513a16aa027ed59b1
Claudio Huang <claudiohuang00@gmail.com>
2023-06-21 00:10:14 +0200

    Aggiunte immagini e slide

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cibando';
+  nome = 'Claudio';
+  label = 'logo di cibando';
+
+  allievi = [
+    {
+      nome: 'Claudio',
+      cognome: 'Huang',
+      eta: '23',
+    },
+    {
+      nome: 'Sid',
+      cognome: 'Azul',
+      eta: '23',
+    },
+    {
+      nome: 'Mario',
+      cognome: 'Rossi',
+      eta: '25',
+    },
+    {
+      nome: 'Marco',
+      cognome: 'Verdi',
+      eta: '29',
+    }
+  ];
+
+  percorso = '../assets/images/imageBg-';
+  images = [
+    {id: 1, label: 'Spaghetti al pomodoro'},
+    // {id: 2, label: 'Tagliato al manzo'},
+    // {id: 3, label: 'Tiramisù classico'},
+  ];
 }
